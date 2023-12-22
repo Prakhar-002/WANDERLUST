7c54d315ed7958e823ff3353bfbadfa42987b7fc
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2023-12-23 01:27:32 +0530

    ADDING Project Files

--- MajorProject.js
+++ MajorProject.js
@@ -34,7 +34,7 @@ app.use(express.static(path.join(__dirname ,"public")));
 const store = MongoStore.create({
       mongoUrl : dbURL,
       crypto : {
-            secret : "mysupersecretcode",
+            secret : process.env.SECRET,
       },
       touchAfter : 24 * 60 * 60 , // in seconds...
 });
@@ -45,7 +45,7 @@ store.on("error" , () =>{
 
 const sessionOptions = {
       store,
-      secret: "mysupersecretcode",
+      secret: process.env.SECRET,
       resave: false,
       saveUninitialized : true,
       cookie :{
